a0e69f68b25ac473495baaea06c0acd06f138fdc
Vinay <vinay.more89@gmail.com>
2016-01-15 09:03:40 -0500

    Create PlayerInterface.java

---
+++ PlayerInterface.java
@@ -0,0 +1,21 @@
+/* 
+ * PlayerInterface.java 
+ * 
+ * Version: PlayerInterface.java,v 1.0 2015/09/20 21:05:46
+ * 
+ */
+
+/**
+ * Interface for player class
+ *
+ * @author Vinay Vasant More
+ *
+ */
+public interface PlayerInterface {
+
+	public char getGamePiece();
+
+	public String getName();
+
+	public int nextMove();
+}
